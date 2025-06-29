08b4c4849c68355713ee381e9f5f572693125887
YucelPehlevan <yucel.pehlevan@bahcesehir.edu.tr>
2025-06-29 15:02:20 +0300

    Update Variables.h

--- Variables.h
+++ Variables.h
@@ -3,7 +3,7 @@ namespace Variables {
     constexpr double windowWidth = 1800.0;
     constexpr double windowHeight = 1000.0;
     constexpr double positionX = 10.0;
-    inline double timeScale = 3.0;  // Now only affects visual speed
+    inline double timeScale = 3.0;  // affecting only visual speed
 
     // Setting Projectile
     inline double userSpeed = 50;
@@ -14,7 +14,7 @@ namespace Variables {
     inline double resistanceCoefficient = 0.02;
     inline double gravity = -9.807;
 
-    // Global deðiþkenler için inline (C++17 ve sonrasý)
+    // inline for global variables (min C++17 required)
     inline double totalTime = 0.0;
     inline double timeAccumulator = 0.0;
     inline double firstTouchTime = 0.0;  // variable for first touch to the ground
